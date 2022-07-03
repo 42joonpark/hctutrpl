a3e7327c50f6c39b3711ed869213df4ce6a431f4
bagmeg <42.4.joonpark@gmail.com>
2022-07-03 22:56:24 +0900

    1. 모듈화 2. GET, PUT, DELETE 확인 3. basicAuth username, password 추가

--- cmd/hctutrpl/app/app.go
+++ cmd/hctutrpl/app/app.go
@@ -1,39 +1,126 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 )
+
+type userType struct {
+	name     string
+	password string
+}
+
+type OptionFlag struct {
+	Method   *string
+	UrlInput *string
+	Output   *string
+	Data     *string
+	Header   *string
+	User     *string
+}
 
 type App struct {
 	Url    string
 	Method string
+	Header http.Header
+	u      *url.URL
+	res    *http.Response
+	body   []byte
 }
 
-func (a *App) Run() {
-	u, err := url.ParseRequestURI(a.Url)
+func (a *App) addHeader(header *string) error {
+	var h map[string]interface{}
+
+	err := json.Unmarshal([]byte(*header), &h)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
+	for key, value := range h {
+		a.Header.Add(key, fmt.Sprintf("%v", value))
+	}
+
+	return nil
+}
+
+func (a *App) addUser(u *string, req *http.Request) error {
+	var user userType
+	if len(*u) > 0 && strings.Contains(*u, ":") {
+		strs := strings.Split(*u, ":")
+		user.name = strs[0]
+		user.password = strs[1]
+
+		req.SetBasicAuth(user.name, user.password)
+	}
+
+	return nil
+}
+
+func (a *App) parseRequestUrl() error {
+	u, err := url.ParseRequestURI(a.Url)
+	if err != nil {
+		return err
+	}
+	a.u = u
+	return nil
+}
+
+func (a *App) sendRequest(options *OptionFlag) error {
 	var req = &http.Request{
 		Method: a.Method,
-		URL:    u,
+		URL:    a.u,
+		Header: a.Header,
+	}
+
+	err := a.addUser(options.User, req)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
+		return err
+	}
+	a.res = res
+	return nil
+}
+
+func (a *App) readBody() error {
+	resBody, err := ioutil.ReadAll(a.res.Body)
+	if err != nil {
+		return err
+	}
+
+	a.body = resBody
+	return nil
+}
+
+func (a *App) Run(options *OptionFlag) {
+	err := a.addHeader(options.Header)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	resBody, err := ioutil.ReadAll(res.Body)
+	err = a.parseRequestUrl()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = a.sendRequest(options)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = a.readBody()
 	if err != nil {
 		fmt.Printf("client: could not read response body: %s\n", err)
 		os.Exit(1)
 	}
-	fmt.Printf("client: response body: %s\n", resBody)
+	fmt.Printf("client: response body: %s\n", a.body)
 }
--- cmd/hctutrpl/main.go
+++ cmd/hctutrpl/main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"flag"
 	"hctutrpl/cmd/hctutrpl/app"
+	"net/http"
 )
 
 var (
 	method   = flag.String("method", "GET", "http request method")
 	urlInput = flag.String("url", "http://127.0.0.1:8080", "request URL")
 	output   = flag.String("output", "", "file to store request result")
+	data     = flag.String("data", "'{}'", "data to send")
+	header   = flag.String("header", `{"Content_Type": "application/json"}`, "header content type")
+	user     = flag.String("user", "", "basic authorize user")
 )
 
 func init() {
 	flag.StringVar(method, "m", "GET", "http request method")
-	flag.StringVar(urlInput, "u", "http://127.0.0.1:8080", "request URL")
+	flag.StringVar(urlInput, "U", "http://127.0.0.1:8080", "request URL")
 	flag.StringVar(output, "o", "", "file to store request result")
+	flag.StringVar(data, "d", "{}", "data to send")
+	flag.StringVar(header, "H", `{"Content_Type": "application/json"}`, "header content type")
+	flag.StringVar(user, "u", "", "basic authorize user")
 	flag.Parse()
 }
 
@@ -22,7 +29,15 @@ func main() {
 	a := app.App{
 		Url:    *urlInput,
 		Method: *method,
+		Header: make(http.Header),
 	}
-
-	a.Run()
+	options := app.OptionFlag{
+		Method:   method,
+		UrlInput: urlInput,
+		Output:   output,
+		Data:     data,
+		Header:   header,
+		User:     user,
+	}
+	a.Run(&options)
 }
